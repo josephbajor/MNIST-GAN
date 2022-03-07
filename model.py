638397a03dd299868448b2b64770c34fa2dc316c
josephbajor <josephbajor@gmail.com>
2022-03-07 17:52:09 -0500

    Network outputs. Will swap back over to the conditional generator approach as soon as the Tensorboard issues are cleared up

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class ConvDescriminator(nn.Module):
+class ConditionalConvDescriminator(nn.Module):
 
     def __init__(self, hparams):
         super().__init__()
@@ -29,7 +29,30 @@ class ConvDescriminator(nn.Module):
         return x
 
 
-class BscGenerator(nn.Module):
+class ConvDescriminator(nn.Module):
+    def __init__(self, hparams):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 6, hparams.kernel_size)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, hparams.kernel_size)
+        self.flatten = nn.Flatten(1)
+        self.fc1 = nn.Linear(16 * 4 * 4, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 1)
+        self.sig = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        x = self.sig(x)
+        return x
+
+
+class BasicGenerator(nn.Module):
     def __init__(self, hparams):
         super().__init__()
         self.fc1 = nn.Linear(hparams.z_dim, 64)
--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from utils import rand_label, rand_label_batched
-from model import ConvDescriminator, ConditionalGenerator
+from model import ConvDescriminator, BasicGenerator
 from dataloaders import load_data
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.utils import make_grid
@@ -13,7 +13,7 @@ from torchvision.utils import make_grid
 def train(hparams, load:bool = False):
     savepath = hparams.model_path
     dsc = ConvDescriminator(hparams).to(hparams.device).double()
-    gen = ConditionalGenerator(hparams).to(hparams.device).double()
+    gen = BasicGenerator(hparams).to(hparams.device).double()
 
     if load:
         assert os.path.isfile(savepath), f"Cannot find {savepath}!"
@@ -28,31 +28,34 @@ def train(hparams, load:bool = False):
     
     trainloader, testloader = load_data(hparams)
 
-    torch.autograd.set_detect_anomaly(True)
+    #torch.autograd.set_detect_anomaly(True)
 
     for epoch in range(hparams.epochs):
         total_loss = 0.0
         for label, x_true in trainloader:
             x_true = x_true.to(hparams.device)
-            label = label.to(hparams.device)
-            label = F.one_hot(label)
-            label_gen = rand_label_batched(hparams, num_labels=10)
-            label_gen = label_gen.to(hparams.device)
+            #label = label.to(hparams.device)
+            #label = F.one_hot(label)
+            #label_gen = rand_label_batched(hparams, num_labels=10)
+            #label_gen = label_gen.to(hparams.device)
 
             dsc.zero_grad()
             gen.zero_grad()
-            #z = torch.randn(hparams.batch_size, hparams.z_dim, 1, 1).to(hparams.device)
-            x_gen = gen(label_gen)
+            z = torch.randn(hparams.batch_size, hparams.z_dim).double().to(hparams.device)
+            x_gen = gen(z)
             dsc_real = dsc(x_true)
-            dsc_loss_real = loss_fn(dsc_real, label.double())
+            dsc_loss_real = loss_fn(dsc_real, torch.ones_like(dsc_real))
             dsc_fake = dsc(x_gen)
-            dsc_loss_fake = loss_fn(dsc_fake,label_gen)
+            dsc_loss_fake = loss_fn(dsc_fake,torch.zeros_like(dsc_fake))
             dsc_loss = (dsc_loss_fake + dsc_loss_real) / 2
-            #dsc_loss.backward(retain_graph=True)
-            dsc_loss.backward()
+            dsc_loss.backward(retain_graph=True)
+            #dsc_loss.backward()
             dsc_opt.step()
 
-            gen_loss = loss_fn(dsc_fake, label_gen)
+            #backward() enacts an inplace operation on the invovled variables so we must create new versions of anything used above if we intend to use backward again on the same graph
+            #Will find a better solution for this. What is usually done for multi-network systems like GANs?
+            dsc_fake = dsc(x_gen)
+            gen_loss = loss_fn(dsc_fake, torch.ones_like(dsc_fake))
             gen_loss.backward()
             gen_opt.step()
 
@@ -61,7 +64,7 @@ def train(hparams, load:bool = False):
         print(f"\nLoss for Epoch {epoch+1}: {total_loss}")
 
         with torch.no_grad():
-            x_gen = gen(label_gen)
+            x_gen = gen(z)
             img_grid = make_grid(x_gen)
             tboard_writer.add_image("generated", img_grid, global_step = epoch)
 
